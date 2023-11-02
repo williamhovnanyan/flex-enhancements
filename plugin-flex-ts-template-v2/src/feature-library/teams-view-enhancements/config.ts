afa8a39fef099b7cd7ef05c5d6ea71c09b269394
William Hovnanyan <williamhovnanyan@gmail.com>
2023-11-02 09:16:15 -0500

    Initial commit

---
+++ plugin-flex-ts-template-v2/src/feature-library/teams-view-enhancements/config.ts
@@ -0,0 +1,51 @@
+import { getFeatureFlags } from '../../utils/configuration';
+import TeamsViewEnhancementsConfig from './types/ServiceConfiguration';
+
+const {
+  enabled = false,
+  highlight_handle_time = true,
+  handle_time_warning_threshold = 180,
+  handle_time_exceeded_threshold = 300,
+  display_task_queue_name = true,
+} = (getFeatureFlags()?.features?.teams_view_enhancements as TeamsViewEnhancementsConfig) || {};
+
+const {
+  team = true,
+  department = false,
+  location = false,
+  agent_skills = true,
+} = getFeatureFlags().features?.teams_view_enhancements?.columns || {};
+
+const { teams = [] } = getFeatureFlags().common || {};
+
+export const isFeatureEnabled = () => {
+  return enabled;
+};
+export const isTeamColumnEnabled = () => {
+  return enabled && team;
+};
+export const isDepartmentColumnEnabled = () => {
+  return enabled && department;
+};
+export const isLocationColumnEnabled = () => {
+  return enabled && location;
+};
+export const isAgentSkillsColumnEnabled = () => {
+  return enabled && agent_skills;
+};
+export const isHTHighlightEnabled = () => {
+  return enabled && highlight_handle_time;
+};
+export const getHTWarningThreshold = () => {
+  return handle_time_warning_threshold;
+};
+export const getHTExceededThreshold = () => {
+  return handle_time_exceeded_threshold;
+};
+export const isDisplayTaskQueueNameEnabled = () => {
+  return enabled && display_task_queue_name;
+};
+
+export const getTeams = () => {
+  return teams;
+};
---
+++ plugin-flex-ts-template-v2/src/feature-library/teams-view-enhancements/types/ServiceConfiguration.ts
@@ -0,0 +1,14 @@
+export default interface TeamsViewEnhancementsConfig {
+  enabled: boolean;
+  highlight_handle_time: boolean;
+  handle_time_warning_threshold: number;
+  handle_time_exceeded_threshold: number;
+  display_task_queue_name: boolean;
+  columns: {
+    team: boolean;
+    department: boolean;
+    location: boolean;
+    agent_skills: boolean;
+    capacity: boolean;
+  };
+}
